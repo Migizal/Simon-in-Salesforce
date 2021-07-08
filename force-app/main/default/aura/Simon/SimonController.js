95314ada519ec175f50c749146632e282de4e570
Miguel McCrary <migmccrary@gmail.com>
2021-07-08 18:36:02 -0500

    Added more array attributes, colored buttons to match a simon device and create variable for the method when a button is pushed

--- force-app/main/default/aura/Simon/SimonController.js
+++ force-app/main/default/aura/Simon/SimonController.js
@@ -5,6 +5,18 @@
 
     //handles
     SimonButtonClick : function(component, event, helper) {
+        let simonPattern = component.get("v.SimonPattern");
+        let userPattern = component.get("v.UserPattern");
+        let buttonValue = event.getSource().get("v.value");
+
+        userPattern.push(buttonValue);
+
+        console.log(userPattern);
+
+        //if(userPattern.length ==)
+        //console.log(buttonEvent);
+
+        
 
     }
 })
