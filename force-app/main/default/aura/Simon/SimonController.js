1ee8566d805b797fe2da5c8ad2b3eac890e7489c
Miguel McCrary <migmccrary@gmail.com>
2021-07-09 07:18:43 -0500

    Simon Pattern shows

--- force-app/main/default/aura/Simon/SimonController.js
+++ force-app/main/default/aura/Simon/SimonController.js
@@ -1,14 +1,36 @@
 ({
     StartSimon : function(component, event, helper) {
         let emptyArray = [];
-        let simonPattern = component.get("v.SimonPattern");
+        //let simonPattern = component.get("v.SimonPattern");
+        let simonPattern = ['green', 'blue', 'red', 'red', 'yellow'];
         let buttonColors = component.get("v.ButtonColors");
-
+        //const timer = ms => new Promise(res => setTimeout(res, ms));
         component.set("v.SimonPattern", emptyArray);
-        simonPattern.push(buttonColors[Math.floor(Math.random() * buttonColors.length)]);
-        component.set("v.SimonPatternHolder", simonPattern);
-        console.log(simonPattern);
+        //simonPattern.push(buttonColors[Math.floor(Math.random() * buttonColors.length)]);
 
+        let j = 0;
+        //show simon pattern
+        function myLoop(){
+            setTimeout(function(){
+                let currentPatternButton = component.find(simonPattern[j]);
+                $A.util.removeClass(currentPatternButton, "slds-transition-hide");
+                $A.util.removeClass(currentPatternButton, "slds-transition-show");
+                console.log("Here: " + currentPatternButton);
+                $A.util.addClass(currentPatternButton, "slds-transition-hide");
+                setTimeout(function() {
+                    $A.util.addClass(currentPatternButton, "slds-transition-show");;
+                }, 750);
+                
+                j++;
+                if(j < simonPattern.length){
+                    myLoop();
+                }
+            }, 1250)
+        }
+        myLoop();
+
+            component.set("v.SimonPatternHolder", simonPattern);
+            console.log(simonPattern);
     },
 
     //handles
