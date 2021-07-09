9a3e70575be90c6968f2dc87e2f69a8e9978b5e2
Miguel McCrary <migmccrary@gmail.com>
2021-07-08 20:50:21 -0500

    Simon will increase it's pattern if user pattern matches

--- force-app/main/default/aura/Simon/SimonController.js
+++ force-app/main/default/aura/Simon/SimonController.js
@@ -15,50 +15,38 @@
     SimonButtonClick : function(component, event, helper) {
         let emptyArray = [];
         let simonPatternHolder = component.get("v.SimonPatternHolder");
-        //let userPatternHolder = component.get("v.UserPatternHolder");
-        //let simonPattern = component.get("v.SimonPattern");
         let userPattern = component.get("v.UserPattern");
         let buttonValue = event.getSource().get("v.value");
-        //let patternMatch = true;
         userPattern.push(buttonValue);
+        console.log(userPattern);
+        //let userPatternHolder = component.get("v.UserPatternHolder");
+        //let simonPattern = component.get("v.SimonPattern");
+        let patternMatch = true;
         //UserPatternHolder.push(userPattern);
 
-        console.log(userPattern);
-
         if(userPattern.length == simonPatternHolder.length){
+
             for(let i = 0; i < simonPatternHolder.length; i++){
+
                 if(userPattern[i] == simonPatternHolder[i] && i+1 == simonPatternHolder.length){
-                    console.log("Pattern Fully Matched...Next Level");
-                    
+                    console.log("Pattern Fully Matched");
 
                 }else if(userPattern[i] == simonPatternHolder[i]){
                     continue;
 
                 }
                  else if(userPattern[i] != simonPatternHolder[i]) {
-                    console.log("Pattern Failed...Retry");
-                    //patternMatch == false;
+                     console.log("Pattern Failed...Retry");
+                     //display message asking to 'Try Again?' and two options, YES or NO. 
+                     //(toggle helper class with message box and buttons to reset or re-render component)
+                    patternMatch = false;
                     break;
                 }
             }
             component.set("v.UserPattern", emptyArray);
-            console.log("Round Done!");
+            if(patternMatch == true){
+                helper.NextLevel(component, simonPatternHolder, helper);
+            }
         }
-    },
-
-    NextLevel: function(component, event, helper) {
-
-        //Add another value to simonPattern
-        //Play the simon pattern
-        //call
-      /*  let emptyArray = [];
-        let simonPattern = component.get("v.SimonPattern");
-        let buttonColors = component.get("v.ButtonColors");
-
-        component.set("v.SimonPattern", emptyArray);
-        simonPattern.push(buttonColors[Math.floor(Math.random() * buttonColors.length)]);
-        component.set("v.SimonPatternHolder", simonPattern);
-        console.log(simonPattern);
-        */
     },
 })
--- force-app/main/default/aura/Simon/SimonHelper.js
+++ force-app/main/default/aura/Simon/SimonHelper.js
@@ -1,5 +1,16 @@
 ({
-    helperMethod : function() {
-
+    NextLevel : function(component, simonPatternHolder, helper) {
+        let buttonColors = component.get("v.ButtonColors");
+        console.log("Next Level!!!")
+        simonPatternHolder.push(buttonColors[Math.floor(Math.random() * buttonColors.length)]);
+        component.set("v.SimonPatternHolder", simonPatternHolder);
+        console.log("Next Level Pattern Below:");
+        console.log(simonPatternHolder);
+        /*let emptyArray = [];
+        let simonPattern = component.get("v.SimonPattern");
+        component.set("v.SimonPattern", emptyArray);
+        component.set("v.SimonPatternHolder", simonPattern);
+        console.log(simonPattern);
+        */
     }
 })
